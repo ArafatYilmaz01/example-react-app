eef6ce8d38921b0ec13d0678b04e28ceb49f27df
Arafat Yilmaz <yilmaz.arafat@gmail.com>
2022-08-07 17:25:52 +0300

    UserDetail Page edited

--- src/pages/Dashboard/UserDetail.js
+++ src/pages/Dashboard/UserDetail.js
@@ -1,28 +1,22 @@
 import React, { useState, useEffect } from "react";
-import { useParams,Link,useLocation } from "react-router-dom";
+import { useParams, Link, useLocation } from "react-router-dom";
 
 function UserDetail() {
-    const location=useLocation();
-    const { id } = useParams();
+  const location = useLocation();
+  const { id } = useParams();
   const [user, setUser] = useState(location.state);
 
- 
-  console.log(location.state)
-  
-    useEffect(() => {
-        if(!user.id){
-        fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
-          .then(res => res.json())
-          .then(data => setUser(data));
-        }
-      }, [id,user]);
-  
-  
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .then(res => res.json())
+      .then(data => setUser(data));
+  }, [id, user]);
+
   return (
     <div>
       <h2>User Detail</h2>
-  {user&& <pre>{JSON.stringify(user, null, 2)}</pre> }
-  <Link to={`/users/${Number(id)+1}`}> Next User</Link>
+      {user && <pre>{JSON.stringify(user, null, 2)}</pre>}
+      <Link to={`/users/${Number(id) + 1}`}> Next User</Link>
     </div>
   );
 }
